a3eae66b13bb1e6989d86cbce8483db8494e62cf
Daniel Biedma <info@dbr.io>
2023-03-28 15:59:43 +0200

    Update install-brew-apps.sh

--- scripts/install-brew-apps.sh
+++ scripts/install-brew-apps.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
 echo "📦 Installing brew apps"
+# All apps (This line is repeated because there are dependencies between brew cask and brew)
+brew bundle --file="$DOTFILES_PATH/cfg/Brewfile" || true
+
+echo "😅 Installing brew apps again because it might fail the first time because of dependencies."
+echo "☝️ Check in MacOS Security & Privacy settings if MacOS is not allowing to install any third party software"
 brew bundle --file="$DOTFILES_PATH/cfg/Brewfile"
